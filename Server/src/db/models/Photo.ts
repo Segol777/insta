c00c551167bba2aa5210be2936b30d87fce96ba0
Armagedon <grauberger1@web.de>
2025-03-17 18:07:02 +0100

    Delete Server directory

--- Server/src/db/models/Photo.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const PhotoSchema = new mongoose.Schema({
-    url: { type: String, required: true },
-    post: {type: mongoose.Schema.Types.ObjectId, ref: "Post"},
-});
-
-const Photo = mongoose.model("Photo", PhotoSchema);
-export default Photo;
